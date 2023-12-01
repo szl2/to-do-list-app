4d2d248d6fdf608f15581f22115bc34abfc5dd9e
SZL <zhiyingli@g.ucla.edu>
2023-12-01 12:31:30 -0500

    Implement the add and complete feature of the web app

--- web.py
+++ web.py
@@ -1,14 +1,29 @@
 import streamlit as st
 import functions
 
+
 todos = functions.get_todos()
+
+
+def add_todo():
+    new_todo = st.session_state["new_todo"]
+    if new_todo != '':
+        todos.append(new_todo + "\n")
+        functions.write_todos(todos)
+
 
 st.title("My To-Do App")
 st.subheader("This is my to-do app")
 st.write("You can enter, modify and delete/complete to-dos.")
 
+for idx, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
 
-for todo in todos:
-    st.checkbox(todo)
+    if checkbox:
+        todos.pop(idx)
+        functions.write_todos(todos)
+        st.rerun()
 
-st.text_input(label="", placeholder="Add a new To-Do...")
+st.text_input(label=" ", placeholder="Add a new To-Do...",
+              on_change=add_todo, key="new_todo")
+
